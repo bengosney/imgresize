1aba0d6bd0d1e145edf91816312f30d28a2a190d
Ben Gosney <bengosney@googlemail.com>
2024-08-30 11:11:24 +0100

    feat(gui): reimplement the gui in iced

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::fs;
 use std::fs::File;
 use std::io::BufWriter;
 use std::io::Write;
-use std::{cell::RefCell, rc::Rc};
+use std::sync::atomic::AtomicI32;
+use std::sync::Arc;
 
 use imgsize::ThreadPool;
 use std::path::PathBuf;
@@ -15,15 +16,30 @@ use image::{ExtendedColorType, ImageEncoder};
 use fast_image_resize::images::Image;
 use fast_image_resize::{IntoImageView, Resizer};
 
-use fltk::{app, dialog, prelude::*};
+use iced::widget::{button, column, progress_bar, text};
+use iced::{Alignment, Application, Command, Element, Length, Settings, Subscription};
 
-mod ui {
-    fl2rust_macro::include_ui!("src/window.fl");
+use native_dialog::FileDialog;
+
+#[derive(Default)]
+struct ImageResizer {
+    completed: i32,
+    total: i32,
+    path: Option<PathBuf>,
+    pool: ThreadPool,
+    completed_tracker: Arc<AtomicI32>,
 }
 
-fn resize_image<P: Into<PathBuf>>(img_path: P) {
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    OpenFileDialog,
+    ResizeImages,
+    Tick,
+}
+
+fn resize_image(path: PathBuf) {
     // Read source image from file
-    let mut path: PathBuf = img_path.into();
+    let mut path = path;
     let src_image = ImageReader::open(path.to_str().unwrap())
         .unwrap()
         .decode()
@@ -74,74 +90,86 @@ fn resize_image<P: Into<PathBuf>>(img_path: P) {
     println!("Image {} saved", filename);
 }
 
-#[derive(Debug, Clone)]
-struct Model {
-    count: usize,
-    max: usize,
-}
-impl Model {
-    fn init() -> Self {
-        Self { count: 0, max: 0 }
-    }
-    pub fn inc(&mut self) -> usize {
-        self.count += 1;
-        self.max = std::cmp::max(self.max, self.count);
-        self.max - self.count
-    }
-    pub fn dec(&mut self) -> usize {
-        self.count -= 1;
-        self.max - self.count
-    }
-}
+#[derive(Debug, Clone, Default)]
+struct UiFlags {}
 
-fn main() {
-    app::GlobalState::<Model>::new(Model::init());
-    let app = app::App::default();
-    let pool = ThreadPool::new(2);
+impl Application for ImageResizer {
+    type Message = Message;
+    type Executor = iced::executor::Default;
+    type Theme = iced::Theme;
+    type Flags = UiFlags;
 
-    let mut ui = ui::UserInterface::make_window();
-    let selected_dir = Rc::new(RefCell::new(None));
+    fn new(_flags: UiFlags) -> (Self, Command<Message>) {
+        (ImageResizer::default(), Command::none())
+    }
 
-    ui.resize_btn.set_callback({
-        let selected_dir = selected_dir.clone();
-        let mut ui = ui.clone();
-        move |_| {
-            if let Some(selected_dir) = &*selected_dir.borrow() {
-                let glob_path = format!("{}/*.jpg", selected_dir);
-                let state = app::GlobalState::<Model>::get();
+    fn title(&self) -> String {
+        String::from("Image Resizer")
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        iced::time::every(std::time::Duration::from_millis(500)).map(|_| Message::Tick)
+    }
+
+    fn update(&mut self, message: Self::Message) -> Command<Message> {
+        match message {
+            Message::OpenFileDialog => {
+                self.path = FileDialog::new().show_open_single_dir().unwrap();
+            }
+            Message::ResizeImages => {
+                self.completed_tracker
+                    .store(0, std::sync::atomic::Ordering::SeqCst);
+                self.total = 0;
+                self.completed = 0;
+                let glob_path = format!("{}/*.jpg", self.path.clone().unwrap().to_str().unwrap());
                 for file in glob(&glob_path).expect("Failed to read glob pattern") {
+                    self.total += 1;
+                    let finished = self.completed_tracker.clone();
                     let path = file.unwrap();
-                    let count = state.with(|model| model.inc());
-                    ui.progress
-                        .set_maximum(state.with(|model| model.max) as f64);
-                    ui.progress.set_value(count as f64);
-
-                    println!("{}", path.to_str().unwrap());
-
-                    pool.execute({
-                        let mut ui = ui.clone();
+                    self.pool.execute({
                         move || {
+                            println!("{:?}", path.display());
                             resize_image(path);
-                            let count = app::GlobalState::<Model>::get().with(|model| model.dec());
-                            ui.progress.set_value(count as f64);
+                            finished.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
                         }
                     });
                 }
             }
-        }
-    });
+            Message::Tick => {
+                self.completed = self
+                    .completed_tracker
+                    .load(std::sync::atomic::Ordering::SeqCst);
+            }
+        };
+        Command::none()
+    }
 
-    ui.select_dir_btn.set_callback({
-        let selected_dir = selected_dir.clone();
-        move |_| {
-            let mut dialog =
-                dialog::NativeFileChooser::new(dialog::NativeFileChooserType::BrowseDir);
-            dialog.show();
-            *selected_dir.borrow_mut() = Some(dialog.filename().to_str().unwrap().to_string());
-            ui.selected_dir
-                .set_label(selected_dir.borrow().as_ref().unwrap());
-        }
-    });
+    fn view(&self) -> Element<Self::Message> {
+        column![
+            button("Select Folder")
+                .on_press(Message::OpenFileDialog)
+                .width(Length::Fill),
+            button("Resize Images")
+                .on_press(Message::ResizeImages)
+                .width(Length::Fill),
+            text(format!("Progress: {} of {}", self.completed, self.total)),
+            progress_bar(0.0..=self.total as f32, self.completed as f32).width(Length::Fill),
+        ]
+        .spacing(10)
+        .padding(20)
+        .width(Length::Fill)
+        .align_items(Alignment::Center)
+        .into()
+    }
+}
 
-    app.run().unwrap();
+fn main() -> iced::Result {
+    ImageResizer::run(Settings {
+        window: iced::window::Settings {
+            size: iced::Size::new(400.0, 175.0),
+            resizable: false,
+            ..iced::window::Settings::default()
+        },
+        ..Settings::default()
+    })
 }
